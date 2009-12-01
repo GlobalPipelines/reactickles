ea22f8bd9974a1d388646f26fdd243f29e2676df
ed@edburton.net <ed@edburton.net@4475d6da-6b21-11de-bb38-9f61fe354615>
2009-12-01 16:46:49 +0000

    trying to rename box to hex (but fearful that svnX is making a pigs ear of it all)

---
+++ OpenFrameworks DevReactickle/apps/hex/src/main.cpp
@@ -0,0 +1,15 @@
+#include "ofMain.h"
+#include "box.h"
+
+int main(int argc, char *argv[]) {
+	int w=320;
+	int h=396;
+	int mode=OF_WINDOW;
+#ifdef TARGET_OS_IPHONE
+	mode=OF_FULLSCREEN;
+#endif
+	if (mode==OF_GAME_MODE)
+		w=h=9999;                       // attempts to use highest available resolution on desktop
+	ofSetupOpenGL(w,h, mode);			// <-------- setup the GL context
+	ofRunApp(new box);
+}
